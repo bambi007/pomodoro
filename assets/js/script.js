cd2ea0eed67a37ad4b49f9260b445d20cb171e62
bambi <sarnina@me.com>
2020-09-07 11:55:48 +0200

    Delete bugs. Timer reset functionality and stop button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let startButton = document.getElementsByClassName('start')[0];
 let pauseButton = document.getElementsByClassName('pause')[0];
+let stopButton = document.getElementsByClassName('stop')[0];
 
 startButton.addEventListener("click", function (event) {
     postMessage('start');
@@ -11,7 +12,13 @@ pauseButton.addEventListener("click", function (event) {
     postMessage('pause');
     pauseButton.classList.remove('active')
     startButton.classList.add('active')
+})
 
+stopButton.addEventListener("click", function (event) {
+    postMessage('stop')
+    pauseButton.classList.remove('active')
+    startButton.classList.add('active')
+    postMessage('reset')
 })
 
 let minutes = 24;
@@ -24,34 +31,46 @@ window.addEventListener('message', function receiveMessage(event) {
 
     switch (event.data) {
         case 'start':
-            timer = setInterval(function () {
 
-                if (seconds === -1) {
-                    minutes -= 1;
-                    seconds = 59;
-                }
+            if (timeLeft > 0) {
 
-                if (timeLeft === 0) {
-                    minutes = 0;
-                    seconds = 0;
-                    clearInterval(timer);
-                }
+                timer = setInterval(function () {
+                    if (seconds === -1) {
+                        minutes -= 1;
+                        seconds = 59;
+                    }
 
-                if (seconds >= 10) {
-                    document.getElementById('timer').innerHTML = (minutes + ':' + seconds);
-                } else {
-                    document.getElementById('timer').innerHTML = (minutes + ':0' + seconds);
-                }
+                    if (timeLeft === 0) {
+                        minutes = 0;
+                        seconds = 0;
+                        clearInterval(timer);
+                        pauseButton.classList.remove('active')
+                        startButton.classList.add('active')
+                        // alert("Time's up!")
+                    }
 
-                timeLeft -= 1;
-                seconds -= 1;
-                console.log(timeLeft)
-            }, 20)
+                    if (seconds >= 10) {
+                        document.getElementById('timer').innerHTML = (minutes + ':' + seconds);
+                    } else {
+                        document.getElementById('timer').innerHTML = (minutes + ':0' + seconds);
+                    }
+
+                    timeLeft -= 1;
+                    seconds -= 1;
+                }, 1000)
+            } else {
+                window.location.reload(true);
+            };
 
             break;
 
         case 'pause':
             clearInterval(timer);
+            break;
+
+        case 'stop':
+            timeLeft = 0;
+            window.location.reload(true);
             break;
     };
 });
